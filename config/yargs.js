3a0c571073f54174efbd5af1696585db95ef2c25
Jorge Sánchez <jsanchezmanzano98@gmail.com>
2020-10-06 11:06:04 +0200

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,30 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+
+};
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como completado o pendiente una tarea'
+};
+
+const argv = require('yargs')
+    .command('crear', 'Crear una tarea por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado a completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borra una tarea', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
